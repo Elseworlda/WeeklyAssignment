50b962779180bb65fcf1daaf6f0b87c9ba6f8030
astro <astro@LAPTOP-1P3I4PFK>
2023-02-07 22:35:46 -0500

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmBurger
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles picVeggie.Click
+
+    End Sub
+End Class
